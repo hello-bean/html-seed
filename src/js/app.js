bb61fad50a03a18596637b0f5e82afb653377769
Ben Bailey <bbailey@rcd.cool>
2017-05-21 20:10:26 -0400

    A good start, need to figure out delete and having new buttons be the same as the original

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,38 @@
 // Javascript goes here
+
+var numButtons = 0;
+
+var addDiv = document.getElementById('buttonAdd');
+
+var superButton = document.getElementById('superButton');
+superButton.addEventListener('click', superButtonClicked);
+
+function superButtonClicked() {
+  console.log('Button clicked');
+  
+  //var addDiv = document.getElementById('buttonAdd');
+  var newButton = document.createElement('button');
+
+  newButton.innerHTML = superButton.innerHTML;
+  
+  addDiv.appendChild(newButton);
+  
+  newButton.addEventListener('click', superButtonClicked);
+
+  numButtons ++;
+}
+
+
+var evilButton = document.getElementById('evilButton');
+evilButton.addEventListener('click', evilButtonClicked);
+
+function evilButtonClicked() {
+	console.log('Evil Button clicked');
+
+	var tobeRemoved = addDiv;
+
+	tobeRemoved.parentNode.removeChild(tobeRemoved[numButtons]);
+
+
+}
+
